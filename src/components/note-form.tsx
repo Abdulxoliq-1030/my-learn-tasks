02ba68810df29d1da3fd13696b996591d23e994d
Abdulxoliq <abdulxoliq@Abdulxoliqs-MacBook-Pro.local>
2023-02-22 15:49:54 +0500

    note form codes

--- src/components/note-form.tsx
+++ src/components/note-form.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Form, Stack } from "react-bootstrap";
+import { Button, Col, Form, Row, Stack } from "react-bootstrap";
+import CreatableReactSelect from "react-select/creatable";
 
 interface NoteFormProps {}
 
@@ -7,9 +8,32 @@ const NoteForm: React.FC<NoteFormProps> = () => {
   return (
     <Form>
       <Stack gap={4}>
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
+        <Row>
+          <Col>
+            <Form.Group controlId="title">
+              <Form.Label>Title</Form.Label>
+              <Form.Control required />
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group controlId="tags">
+              <Form.Label>Tags</Form.Label>
+              <CreatableReactSelect isMulti />
+            </Form.Group>
+          </Col>
+        </Row>
+        <Form.Group controlId="markdown">
+          <Form.Label>Body</Form.Label>
+          <Form.Control required as="textarea" rows={15} />
+        </Form.Group>
+        <Stack gap={2} className="justify-content-end" direction="horizontal">
+          <Button type="submit" variant="primary">
+            Save
+          </Button>
+          <Button type="button" variant="outline-secondary">
+            Cancel
+          </Button>
+        </Stack>
       </Stack>
     </Form>
   );
