b4bcff40ad5db71114b9c8a28baac32d1a23124e
Abdulxoliq <abdulxoliq@Abdulxoliqs-MacBook-Pro.local>
2023-02-22 21:37:46 +0500

    note list finish codes

--- src/note-list.tsx
+++ src/note-list.tsx
@@ -1,20 +1,40 @@
-import { useState } from "react";
-import { Button, Col, Form, Row, Stack } from "react-bootstrap";
+import { useMemo, useState } from "react";
+import { Badge, Button, Card, Col, Form, Row, Stack } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import ReactSelect from "react-select";
-import { Tag } from "./App";
+import { Note, Tag } from "./App";
+import styles from "./NoteList.module.css";
 
 type NoteListProps = {
   availableTags: Tag[];
+  notes: Note[];
 };
 
-export function NoteList({ availableTags }: NoteListProps) {
+type SimplifiedNote = {
+  tags: Tag[];
+  title: string;
+  id: string;
+};
+
+export function NoteList({ availableTags, notes }: NoteListProps) {
   const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
   const [title, setTitle] = useState("");
 
+  const filteredNotes = useMemo(() => {
+    return notes.filter((note) => {
+      return (
+        title === "" ||
+        (note.title.toLowerCase().includes(title.toLowerCase()) &&
+          (selectedTags.length === 0 ||
+            selectedTags.every((tag) =>
+              note.tags.some((noteTag) => noteTag.id === tag.id)
+            )))
+      );
+    });
+  }, [title, selectedTags, notes]);
   return (
     <>
-      <Row>
+      <Row className="align-items-center mb-4">
         <Col>
           <h1>Notes</h1>
         </Col>
@@ -62,6 +82,42 @@ export function NoteList({ availableTags }: NoteListProps) {
           </Col>
         </Row>
       </Form>
+      <Row xs={1} sm={2} lg={3} xl={4} className="g-3">
+        {filteredNotes.map((note) => (
+          <Col key={note.id}>
+            <NoteCard id={note.id} title={note.title} tags={note.tags} />
+          </Col>
+        ))}
+      </Row>
     </>
+  );
+}
+
+function NoteCard({ id, title, tags }: SimplifiedNote) {
+  return (
+    <Card
+      as={Link}
+      to={`/${id}`}
+      className={`h-100 text-reset text-decoration-none ${styles.card}`}>
+      <Card.Body>
+        <Stack
+          gap={2}
+          className="align-items-center justify-content-center h-100">
+          <span className="fs-5">{title}</span>
+          {tags.length > 0 && (
+            <Stack
+              gap={1}
+              direction="horizontal"
+              className="justify-content-center flex-wrap">
+              {tags.map((tag) => (
+                <Badge className="text-truncate" key={tag.id}>
+                  {tag.label}
+                </Badge>
+              ))}
+            </Stack>
+          )}
+        </Stack>
+      </Card.Body>
+    </Card>
   );
 }
