5251f6fba02c15dffe43953e618f139b16a20792
Abdulxoliq <abdulxoliq@Abdulxoliqs-MacBook-Pro.local>
2023-02-22 20:38:58 +0500

    note form finish codes

--- src/App.tsx
+++ src/App.tsx
@@ -57,11 +57,24 @@ const App: React.FC<AppProps> = () => {
     });
   }
 
+  function addTag(tag: Tag) {
+    setTags((prev) => [...prev, tag]);
+  }
+
   return (
     <Container className="my-4">
       <Routes>
         <Route path="/" element={<h1>Home</h1>} />
-        <Route path="/new" element={<NewNote onSubmit={onCreateNote} />} />
+        <Route
+          path="/new"
+          element={
+            <NewNote
+              onSubmit={onCreateNote}
+              onAddTag={addTag}
+              availableTags={tags}
+            />
+          }
+        />
         <Route path="/:id">
           <Route index element={<h1>Show</h1>} />
           <Route path="edit" element={<h1>Edit</h1>} />
--- src/new-note.tsx
+++ src/new-note.tsx
@@ -1,16 +1,26 @@
 import React from "react";
-import { NoteData } from "./App";
+import { NoteData, Tag } from "./App";
 import NoteForm from "./note-form";
 
 interface NewNoteProps {
   onSubmit: (data: NoteData) => void;
+  onAddTag: (tag: Tag) => void;
+  availableTags: Tag[];
 }
 
-const NewNote: React.FC<NewNoteProps> = ({ onSubmit }) => {
+const NewNote: React.FC<NewNoteProps> = ({
+  onSubmit,
+  onAddTag,
+  availableTags,
+}) => {
   return (
     <>
       <h1 className="mb-4">New Note</h1>
-      <NoteForm onSubmit={onSubmit} />
+      <NoteForm
+        onSubmit={onSubmit}
+        onAddTag={onAddTag}
+        availableTags={availableTags}
+      />
     </>
   );
 };
